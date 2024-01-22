bcc08d4e3b167d6a49273370f59de8249c031262
Youjung Oh <ooyouj@naver.com>
2024-01-22 17:49:06 +0900

    Feat : Add Optimistic Lock repository

--- src/main/java/com/example/stock/repository/StockRepository.java
+++ src/main/java/com/example/stock/repository/StockRepository.java
@@ -10,8 +10,13 @@ import com.example.stock.domain.Stock;
 
 public interface StockRepository extends JpaRepository<Stock, Long> {
 
+	// Pessimistic Lock
 	@Lock(LockModeType.PESSIMISTIC_WRITE)
 	@Query("select s from Stock s where s.id = :id")
 	Stock findByIdWithPessimisticLock(Long id);
 
+	// Optimistic Lock
+	@Lock(LockModeType.OPTIMISTIC)
+	@Query("select s from Stock s where s.id = :id")
+	Stock findByIdWithOptimisticLock(Long id);
 }
